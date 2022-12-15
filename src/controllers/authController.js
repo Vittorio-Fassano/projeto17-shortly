085759374c9f9ca379c1c0e78f29c40229049c9f
Vittorio <vittfassano@gmail.com>
2022-12-15 12:16:50 -0300

    feat: signUp

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,23 @@
-export async function signUp (req, res) {
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
 
+import { connectionDB } from "../database/db.js";
+
+export async function signUp(req, res) {
+  const { name, email, password } = req.body;
+  const passwordHash = bcrypt.hashSync(password, 10);
+
+  try {
+    await connectionDB.query(
+      `INSERT INTO users (name, email, password) 
+      VALUES ($1, $2, $3);`,
+      [name, email, passwordHash]
+    );
+
+    res.sendStatus(201);
+  } catch (err) {
+    return res.status(422).send(err);
+  }
 }
 
-export async function signIn (req, res) {
-
-}
+export async function signIn(req, res) {}
--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,7 +1,32 @@
-export async function validatingSignUp (req, res, next) {
+import bcrypt from "bcrypt";
+import { connectionDB } from "../database/db.js";
 
+export async function validatingSignUp(req, res, next) {
+  const { email } = req.body;
+  const { error } = signUpSchema.validate(req.body, { abortEarly: false });
+
+  if (error) {
+    return res.status(400).send(error.details.map((detail) => detail.message));
+  }
+
+  try {
+    const emailAlreadyExist = await connectionDB.query(
+      `SELECT * 
+      FROM users 
+      WHERE email = $1;`,
+      [email]
+    );
+
+    if (emailAlreadyExist.rows[0]) {
+      return res.sendStatus(409);
+    }
+
+    next();
+  } catch (err) {
+    return res.sendStatus(500);
+  }
 }
 
-export async function validatingSignIn (req, res, next) {
-
+export async function validatingSignIn(req, res, next) {
+    
 }
