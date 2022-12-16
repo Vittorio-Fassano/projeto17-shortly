521e2cb2c04396ec13140fdc8b267551e3338389
Vittorio <vittfassano@gmail.com>
2022-12-16 16:52:15 -0300

    feat: usersTable & sessionsTable

--- src/tables/tables.sql
+++ src/tables/tables.sql
@@ -1,2 +1,15 @@
-'users'
-'sessions'
+CREATE TABLE "users" (
+    "id" SERIAL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL UNIQUE,
+    "password" TEXT NOT NULL,
+    "confirmPassword" TEXT NOT NULL,
+    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE "sessions" (
+    "id" SERIAL PRIMARY KEY,
+    "token" TEXT NOT NULL UNIQUE,
+    "userId" INTEGER NOT NULL REFERENCES "users"("id"),
+    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
