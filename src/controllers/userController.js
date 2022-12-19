69b4fc508ed9d1a68162878f00b59b70daf6d58d
Vittorio <vittfassano@gmail.com>
2022-12-19 16:30:27 -0300

    feat: ranking

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,10 +3,12 @@ import connectionDB from "../database/db.js";
 export async function user(req, res) {
   const { id } = req.params;
   try {
-    //it's missing to add the total number of views of the user in all the posts together
+    //it's missing add the total number of views (visitCount) of the user in all the posts together
     const userId = await connectionDB.query(
       `SELECT users.id, users.name 
       FROM users 
+      JOIN urls 
+      ON urls."userId" = users.id 
       WHERE users.id = $1 
       GROUP BY users.id;`,
       [id]
@@ -14,16 +16,34 @@ export async function user(req, res) {
     const user = userId.rows[0];
 
     const shortId = await connectionDB.query(
-        `SELECT urls.id, urls."shortUrl", urls.url, urls.views AS "visitCount"
-        FROM urls 
-        WHERE "userId" = $1;`,
-        [id]
-      );
-      const short = shortId.rows;
+      `SELECT urls.id, urls."shortUrl", urls.url, urls.views AS "visitCount" 
+      FROM urls 
+      WHERE "userId" = $1;`,
+      [id]
+    );
+    const short = shortId.rows;
 
-      const result = {...user, short}
-      return res.status(200).send(result);
+    const result = { ...user, short };
+    return res.status(200).send(result);
   } catch (err) {
     return res.sendStatus(500);
+  }
+}
+
+export async function ranking(req, res) {
+  try {
+    const ranking = await connectionDB.query(
+      `SELECT users.id, users.name, COUNT (urls.url) AS "linksCount" 
+      FROM users 
+      JOIN urls 
+      ON users.id = urls."userId" 
+      GROUP BY users.id 
+      ORDER BY "linksCount" DESC 
+      LIMIT 10;`
+    );
+
+    return res.status(200).send(ranking.rows);
+  } catch (err) {
+    return res.sendStatus(422);
   }
 }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,9 +2,13 @@ import {Router} from 'express';
 
 import { validatingSession } from "../middlewares/sessionMiddleware.js";
 import { validatingUser } from '../middlewares/userMiddleware.js';
-import { user } from '../controllers/userController.js';
+import { user, ranking } from '../controllers/userController.js';
 
 const userRouter = Router();
-userRouter.get("/users/me", validatingSession, validatingUser, user);
+//route bellow with problem to getById by params in validatingUser middleware and pass to user controller
+userRouter.get("/users/me", validatingSession, validatingUser, user); 
+
+//route bellow is incomplete, because it's ranking only by linkCount, missing ranking by visitCount
+userRouter.get("/ranking", ranking); 
 
 export default userRouter;
